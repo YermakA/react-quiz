1e492f145cda85a01324a982a231e4d15874cadf
YermakA <ermak.080@mail.ru>
2022-05-06 22:19:23 +0800

    added  the implementation of questions, the context and QuizProvider were created

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Question from './Question';
+import { QuizContext } from '../contexts/quiz';
+
+
+
 
 const Quiz = () => {
+    const [quizState, dispatch] = useContext(QuizContext);
+    console.log(quizState);
+
     return (
         <div className='quiz'>
             <div>
                 <div className='score'>Question 1/8</div>
-                <Question />
-                <div className='next-button'>Next question</div>
+                <Question questions={quizState.questions} />
+                <div
+                    className='next-button'
+                    onClick={() => dispatch({ type: "NEXT_QUESTION" })}
+                >Next question</div>
             </div>
         </div>
     );
